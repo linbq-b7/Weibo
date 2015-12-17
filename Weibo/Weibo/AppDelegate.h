5dfb4579899acb53246932b1040208c931876c91
lbq <lbq@linbqmac.local>
2015-12-18 00:40:57 +0800

    新建微博项目

---
+++ Weibo/Weibo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Weibo
+//
+//  Created by lbq on 15/12/18.
+//  Copyright © 2015年 linbq-b7. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
